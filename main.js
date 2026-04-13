c9a79b9861f78b83019e20c4a5b6e409c3a8ca88
Anna <124587762+genepearl@users.noreply.github.com>
2026-04-13 15:30:34 +0200

    split html in three files

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+            const observerOptions = {
+                root: null,
+                rootMargin: '0px',
+                threshold: 0.15
+            };
+
+            const observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('active');
+                    }
+                });
+            }, observerOptions);
+
+            document.querySelectorAll('.reveal, .diagnose-container').forEach((el) => {
+                observer.observe(el);
+            });
+        });
